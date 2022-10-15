4abe75bbca9388aac1cdaa67d58f6c5131231c18
shikoto <1sh1m1da1@gmail.com>
2022-10-15 01:34:53 +0000

    First commit

--- src/ChoosingSection.jsx
+++ src/ChoosingSection.jsx
@@ -10,20 +10,20 @@ function ChoosingSection({setPlayerChoice,options,setComputerChoice}) {
 
     return( 
         <section id="choosing-section">
-            <Link to={`deliberation-screen`}>
-                <button onClick={handleChoice} value="Scissors" className="option-button" id="scissors-button"></button>
+            <Link to={`deliberation-screen`} aria-label="scissors-button">
+                <button onClick={handleChoice} value="Scissors" className="option-button" id="scissors-button" aria-label="scissors-button"></button>
             </Link>
-            <Link to={`deliberation-screen`}>
-                <button onClick={handleChoice} value="Paper" className="option-button" id="paper-button"></button>
+            <Link to={`deliberation-screen`} aria-label="paper-button">
+                <button onClick={handleChoice} value="Paper" className="option-button" id="paper-button" aria-label="paper-button"></button>
             </Link>
-            <Link to={`deliberation-screen`}>
-                <button onClick={handleChoice} value="Rock" className="option-button" id="rock-button"></button>
+            <Link to={`deliberation-screen`} aria-label="rock-button">
+                <button onClick={handleChoice} value="Rock" className="option-button" id="rock-button" aria-label="rock-button"></button>
             </Link>
-            <Link to={`deliberation-screen`}>
-                <button onClick={handleChoice} value="Lizard" className="option-button" id="lizard-button"></button>
+            <Link to={`deliberation-screen`} aria-label="lizard-button">
+                <button onClick={handleChoice} value="Lizard" className="option-button" id="lizard-button" aria-label="lizard-button"></button>
             </Link>
-            <Link to={`deliberation-screen`}>
-                <button onClick={handleChoice} value="Spock" className="option-button" id="spock-button"></button>
+            <Link to={`deliberation-screen`} aria-label="spock-button">
+                <button onClick={handleChoice} value="Spock" className="option-button" id="spock-button" aria-label="spock-button"></button>
             </Link>
         </section>
     )
